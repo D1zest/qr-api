94df008ba673ec7c60e7783c496f01acc555a2c9
다이제스트 <handsomejudong@gmail.com>
2024-10-30 22:01:45 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, request, send_file
+import qrcode
+from io import BytesIO
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def main():
+    return 'https://github.com/D1zest/qr-api/tree/main'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/g')
+def g():
+    data = request.args.get('data')
+    size = request.args.get('size', '200*200')
+
+    if not data:
+        return 'https://github.com/D1zest/qr-api/tree/main', 400
+
+    try:
+        width, height = map(int, size.split('*'))
+    except ValueError:
+        return 'https://github.com/D1zest/qr-api/tree/main', 400
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    img = img.resize((width, height))
+
+    buffer = BytesIO()
+    img.save(buffer, 'PNG')
+    buffer.seek(0)
+
+    return send_file(buffer, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
